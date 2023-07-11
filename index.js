acf8f0bcf3f297a55fa9c26a4d6d085e961121a6
BiancaMe <bianquis0410@gmail.com>
2023-07-10 21:22:53 -0300

    Create th funtion AddBook .Update index.html and index.js. Config linters errors

---
+++ index.js
@@ -0,0 +1,59 @@
+/* eslint-disable max-classes-per-file */
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+class listBooks {
+  static getBooks() {
+    let books = [];
+    if (localStorage.getItem('books') != null) { books = JSON.parse(localStorage.getItem('books')); }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = listBooks.getBooks();
+    const arr = Array.from(books);
+    arr.push(book);
+    localStorage.setItem('books', JSON.stringify(arr));
+    this.addBookDOM(book);
+  }
+
+  static addBookDOM(book) {
+    const newBook = document.createElement('div');
+    newBook.setAttribute('class', 'book');
+    document.querySelector('#list-books').append(newBook);
+
+    const title = document.createElement('h3');
+    title.setAttribute('class', 'title');
+    const txtTitle = document.createTextNode(book.title);
+    title.appendChild(txtTitle);
+    newBook.appendChild(title);
+
+    const author = document.createElement('h4');
+    author.setAttribute('class', 'author');
+    const txtAuthor = document.createTextNode(book.author);
+    author.appendChild(txtAuthor);
+    newBook.appendChild(author);
+
+    const btnAdd = document.createElement('button');
+    btnAdd.setAttribute('class', 'btn-remove');
+    newBook.appendChild(btnAdd);
+  }
+}
+
+document.querySelector('#form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newTitle = document.querySelector('#title').value;
+  const newAuthor = document.querySelector('#author').value;
+  const book = new Book(newTitle, newAuthor);
+  listBooks.addBook(book);
+});
+
+window.addEventListener('load', (e) => {
+  e.preventDefault();
+  const list = listBooks.getBooks();
+  list.forEach((book) => listBooks.addBookDOM(book));
+});
